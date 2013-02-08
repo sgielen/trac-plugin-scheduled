d5d6609bbfeeeb2d2176269985fea8eb7c36a230
Sjors Gielen <sjors@sjorsgielen.nl>
2013-02-08 23:26:28 +0100

    Fully implement ITemplateProvider interface.

--- scheduled/scheduled.py
+++ scheduled/scheduled.py
@@ -5,6 +5,7 @@ from trac.web.chrome import INavigationContributor, ITemplateProvider
 from trac.web.main import IRequestHandler
 from trac.util.translation import _
 from genshi.builder import tag
+from pkg_resources import resource_filename
 
 class Scheduled(Component):
 	implements(INavigationContributor, IRequestHandler, ITemplateProvider)
@@ -25,5 +26,6 @@ class Scheduled(Component):
 
 	# ITemplateProvider: Provide templates for the pages used in process_request
 	def get_templates_dirs(self):
-		from pkg_resources import resource_filename
 		return [resource_filename(__name__, 'templates')]
+	def get_htdocs_dirs(self):
+		return []
